fbf0eb8d00db82f49efbcb1c9e7477bad13fa5aa
PC677 <shoxruxsharipov1105@gmail.com>
2023-02-08 11:17:33 +0500

    test pro

--- app/src/main/java/uz/itschool/myapplication/MainActivity.kt
+++ app/src/main/java/uz/itschool/myapplication/MainActivity.kt
@@ -10,69 +10,68 @@ import android.widget.ImageButton
 import android.widget.RadioButton
 import android.widget.RadioGroup
 import android.widget.TextView
+import uz.itschool.myapplication.model.Test
 import java.util.Random
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var question:TextView
+    lateinit var question: TextView
     lateinit var group: RadioGroup
-    lateinit var answer1:RadioButton
-    lateinit var answer2:RadioButton
-    lateinit var answer3:RadioButton
-    lateinit var answer4:RadioButton
-    lateinit var finish:Button
-    lateinit var next:ImageButton
-    lateinit var prev:ImageButton
-    var n = 1
-    var array = Array<Int>(4,{-1})
+    lateinit var answer1: RadioButton
+    lateinit var answer2: RadioButton
+    lateinit var answer3: RadioButton
+    lateinit var answer4: RadioButton
+    lateinit var finish: Button
+    lateinit var next: ImageButton
+    lateinit var prev: ImageButton
+    var count = 0
+    var list = mutableListOf<Test>()
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         question = findViewById(R.id.question)
-        group = findViewById(R.id.radioGroup)
         answer1 = findViewById(R.id.answer1)
         answer2 = findViewById(R.id.answer2)
         answer3 = findViewById(R.id.answer3)
         answer4 = findViewById(R.id.answer4)
-        question = findViewById(R.id.question)
-        finish = findViewById(R.id.finish)
         next = findViewById(R.id.next)
         prev = findViewById(R.id.previus)
+        group = findViewById(R.id.radioGroup)
+        var index = 0
+        list.add(Test("1*1", "1", "2", "3", "4", false))
+        list.add(Test("2*2", "12", "4", "9", "13", false))
+        list.add(Test("3*3", "5", "6", "17", "9", false))
+        list.add(Test("4*5", "10", "20", "30", "40", false))
 
-
-
+        createTest(index)
         next.setOnClickListener {
-            question.text = question.text.dropLast(1)
-            n++
-            question.text = question.text.toString() + n.toString()
-            answer1.text = answer1.text.dropLast(1).toString() + (n+3).toString()
-            answer2.text = answer2.text.dropLast(1).toString() + (n+4).toString()
-            answer3.text = answer3.text.dropLast(1).toString() + (n+5).toString()
-            answer4.text = answer4.text.dropLast(1).toString() + (n+6).toString()
-
-            var b = findViewById<RadioButton>(group.checkedRadioButtonId)
-            array[n] = b.text.lastIndex.toInt()
-
-            if (n == 5){
-                finish.visibility = View.VISIBLE
+            if (index < list.size - 1) {
+                index++
+                createTest(index)
             }
-
         }
 
         prev.setOnClickListener {
-            question.text = question.text.dropLast(1)
-            n--
-            question.text = question.text.toString() + n.toString()
-            answer1.text = answer1.text.dropLast(1).toString() + (n+3).toString()
-            answer2.text = answer2.text.dropLast(1).toString() + (n+4).toString()
-            answer3.text = answer3.text.dropLast(1).toString() + (n+5).toString()
-            answer4.text = answer4.text.dropLast(1).toString() + (n+6).toString()
-//            var a = group.checkedRadioButtonId
-//            var b = findViewById<RadioButton>(a)
-//            b.isChecked = false
-        }
+            if (index < list.size - 1 && index != 0) {
+                index--
+                createTest(index)
+                if (group.checkedRadioButtonId != -1){
+                    group.clearCheck()
+                }
+            }
 
+        }
+    }
+
+
+    fun createTest(i: Int) {
+        question.text = list[i].qustion
+        answer1.text = list[i].answer1
+        answer2.text = list[i].answer2
+        answer3.text = list[i].answer3
+        answer4.text = list[i].answer4
+        list[i].status = true
     }
 }
---
+++ app/src/main/java/uz/itschool/myapplication/model/Test.kt
@@ -0,0 +1,5 @@
+package uz.itschool.myapplication.model
+
+class Test(var qustion:String, var answer1:String,var answer2:String,var answer3:String,var answer4:String, var status:Boolean) {
+
+}
